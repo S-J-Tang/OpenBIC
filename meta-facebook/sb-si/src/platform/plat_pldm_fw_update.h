4d095c955bce2281c524b09d25afaed805eef830
Billy.Tang@quantatw.com <Billy.Tang@quantatw.com>
2025-06-26 08:56:40 +0800

    add "Get fw version command vr" command function

--- meta-facebook/sb-si/src/platform/plat_pldm_fw_update.h
+++ meta-facebook/sb-si/src/platform/plat_pldm_fw_update.h
@@ -33,6 +33,6 @@ void load_pldmupdate_comp_config(void);
 void clear_pending_version(uint8_t activate_method);
 bool find_sensor_id_and_name_by_firmware_comp_id(uint8_t comp_identifier, uint8_t *sensor_id,
 						 char *sensor_name);
-int get_aegis_vr_compnt_mapping_sensor_table_count(void);
+int get_si_vr_compnt_mapping_sensor_table_count(void);
 
 #endif /* _PLAT_FWUPDATE_H_ */
