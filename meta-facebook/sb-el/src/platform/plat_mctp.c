b876f028c14e0f0a0ce0ff27fac2b1a92fa64233
danielchang <daniel_chang@quantatw.com>
2026-04-23 16:35:19 +0800

    sb: el: Modify MCTP from i2c to i3c Summary: - Modify MCTP from i2c to i3c Test Plan: - Build code: Pass

--- meta-facebook/sb-el/src/platform/plat_mctp.c
+++ meta-facebook/sb-el/src/platform/plat_mctp.c
@@ -41,17 +41,19 @@ LOG_MODULE_REGISTER(plat_mctp);
 
 uint8_t plat_eid = MCTP_DEFAULT_ENDPOINT;
 
-/*
+
 static mctp_port i3c_port[] = {
 	{
 		.conf.i3c_conf.addr = I2C_ADDR_BMC,
 		.conf.i3c_conf.bus = I3C_BUS_BMC,
 	},
-};*/
+};
 
+/*
 static mctp_port smbus_port[] = {
 	{ .conf.smbus_conf.addr = I2C_ADDR_BIC, .conf.smbus_conf.bus = I2C_BUS_BMC },
 };
+*/
 
 mctp_port plat_mctp_port[] = {
 	{ .channel_target = PLDM,
@@ -61,7 +63,7 @@ mctp_port plat_mctp_port[] = {
 };
 
 mctp_route_entry mctp_route_tbl[] = {
-	{ MCTP_EID_BMC, I2C_BUS_BMC, I2C_ADDR_BMC },
+	{ MCTP_EID_BMC, I3C_BUS_BMC, I2C_ADDR_BMC },
 };
 
 uint8_t MCTP_SUPPORTED_MESSAGES_TYPES[] = {
@@ -72,10 +74,10 @@ uint8_t MCTP_SUPPORTED_MESSAGES_TYPES[] = {
 mctp *find_mctp_by_bus(uint8_t bus)
 {
 	uint8_t i;
-	for (i = 0; i < ARRAY_SIZE(smbus_port); i++) {
-		mctp_port *p = smbus_port + i;
+	for (i = 0; i < ARRAY_SIZE(i3c_port); i++) {
+		mctp_port *p = i3c_port + i;
 
-		if (bus == p->conf.smbus_conf.bus)
+		if (bus == p->conf.i3c_conf.bus)
 			return p->mctp_inst;
 	}
 
@@ -97,8 +99,8 @@ uint8_t get_mctp_info(uint8_t dest_endpoint, mctp **mctp_inst, mctp_ext_params *
 		}
 		if (p->endpoint == dest_endpoint) {
 			*mctp_inst = find_mctp_by_bus(p->bus);
-			ext_params->type = MCTP_MEDIUM_TYPE_SMBUS;
-			ext_params->smbus_ext_params.addr = p->addr;
+			ext_params->type = MCTP_MEDIUM_TYPE_TARGET_I3C;
+			ext_params->i3c_ext_params.addr = p->addr;
 			ext_params->ep = dest_endpoint;
 			rc = MCTP_SUCCESS;
 			break;
@@ -120,8 +122,8 @@ static uint8_t get_mctp_route_info(uint8_t dest_endpoint, void **mctp_inst,
 		const mctp_route_entry *p = mctp_route_tbl + i;
 		if (p->endpoint == dest_endpoint) {
 			*mctp_inst = find_mctp_by_bus(p->bus);
-			ext_params->type = MCTP_MEDIUM_TYPE_SMBUS;
-			ext_params->smbus_ext_params.addr = p->addr;
+			ext_params->type = MCTP_MEDIUM_TYPE_TARGET_I3C;
+			ext_params->i3c_ext_params.addr = p->addr;
 			rc = MCTP_SUCCESS;
 			break;
 		}
@@ -163,10 +165,10 @@ void plat_mctp_init(void)
 	LOG_INF("plat_mctp_init");
 
 	/* init the mctp/pldm instance */
-	for (uint8_t i = 0; i < ARRAY_SIZE(smbus_port); i++) {
-		mctp_port *p = smbus_port + i;
-		LOG_DBG("smbus port %d", i);
-		LOG_DBG("bus = %x, addr = %x", p->conf.smbus_conf.bus, p->conf.smbus_conf.addr);
+	for (uint8_t i = 0; i < ARRAY_SIZE(i3c_port); i++) {
+		mctp_port *p = i3c_port + i;
+		LOG_DBG("i3c port %d", i);
+		LOG_DBG("bus = %x, addr = %x", p->conf.i3c_conf.bus, p->conf.i3c_conf.addr);
 
 		p->mctp_inst = mctp_init();
 		if (!p->mctp_inst) {
@@ -188,7 +190,7 @@ void plat_mctp_init(void)
 			continue;
 		}*/
 		uint8_t rc =
-			mctp_set_medium_configure(p->mctp_inst, MCTP_MEDIUM_TYPE_SMBUS, p->conf);
+			mctp_set_medium_configure(p->mctp_inst, MCTP_MEDIUM_TYPE_TARGET_I3C, p->conf);
 		LOG_DBG("mctp_set_medium_configure %s",
 			(rc == MCTP_SUCCESS) ? "success" : "failed");
 
@@ -207,12 +209,12 @@ int load_mctp_support_types(uint8_t *type_len, uint8_t *types)
 
 uint8_t plat_get_mctp_port_count()
 {
-	return ARRAY_SIZE(smbus_port);
+	return ARRAY_SIZE(i3c_port);
 }
 
 mctp_port *plat_get_mctp_port(uint8_t index)
 {
-	return smbus_port + index;
+	return i3c_port + index;
 }
 
 void plat_i3c_set_pid(void)
